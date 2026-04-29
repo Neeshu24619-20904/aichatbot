a5ed973263563d8df8b8c5f7b7207057063229f2
neeshu <neeshant.24bcs10635@sst.scaler.com>
2026-04-29 23:48:59 +0530

    changed mode

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.post("/chat", async (req, res) => {
     const systemPrompt = personas[persona];
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash", // fast + cheap
+      model: "gemini-3-flash-preview", // fast + cheap
       contents: [
         {
           role: "user",
